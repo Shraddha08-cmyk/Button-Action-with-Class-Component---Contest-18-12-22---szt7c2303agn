1d198bf26dd656d13dd886a0ed96da722dbe4308
Shraddha08-cmyk <shraddhatiwari8175@gmail.com>
2022-12-18 18:25:54 +0530

    new

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,19 @@
 import React, {Component, useState} from "react";
-import '../styles/App.css';
+import "./../styles/App.css";
 
-function App() {
-	const[displayMessage, setDisplayMessage] = useState(false);
-	
-	return (
-		<div id="main">
+function App() 
+{
+  const [displayMessage, setDisplayMessage] = useState(false);
+
+  return (
+	<div id="main">
 		{
-		(displayMessage === false) ? (null) : (<p id="para>Hello, I've learnt to you use the full-stack evaluation tool. This makes me so happy<
-	 }
-                <button id="click" onClick={()=>setDisplayMessage(true)}Click me</button>
-                </div>
-    );
+    		(displayMessage === false)?(null):(<p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>)
+    	}
+		<button id="click" onClick={()=>setDisplayMessage(true)}>Click me</button>
+    </div>
+  );
 }
 
-export default App;
 
+export default App;
